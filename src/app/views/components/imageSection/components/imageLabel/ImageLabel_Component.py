29e0119e2bf9de00d5656cd3d5dc47139119c373
krosbift <torifaoficial@gmail.com>
2024-10-23 00:40:38 -0500

    error de la muestra de imagen corregido

--- src/app/views/components/imageSection/components/imageLabel/ImageLabel_Component.py
+++ src/app/views/components/imageSection/components/imageLabel/ImageLabel_Component.py
@@ -10,10 +10,10 @@ class ImageLabelComponent(tk.Label):
 
   def init_config(self):
     super().__init__(self.section, highlightthickness=1)
-    self.place(anchor="e")
+    self.place(x=0, y=0, relwidth=0.7, relheight=1, anchor="nw")
 
   def on_resize(self, _):
-    self.place_configure(anchor="e")
+    self.place_configure(x=0, y=0, relwidth=0.7, relheight=1, anchor="nw")
 
   def update_image(self):
     self.config(image=self.controller.image)
